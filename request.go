4643231ef5ba293eb89e7e54cb09959e144e242a
abdulahwahdi <abdulah.wahdi@gmail.com>
2021-02-01 12:51:30 +0700

    Update Payload for Update Status to Cicil - Add ShipmentNo and ShipmentProvider

--- request.go
+++ request.go
@@ -1,77 +1,78 @@
 package cicil
 
 type CheckoutRequest struct {
-	Transaction Transaction  `json:"transaction"`
-	PushURL     string       `json:"push_url"`
-	RedirectURL string       `json:"redirect_url"`
-	Buyer       Buyer        `json:"buyer"`
-	Shipment    Shipment     `json:"shipment"`
-	SellerList  []SellerList `json:"seller_list,omitempty"`
-	BackURL     string       `json:"back_url"`
+    Transaction Transaction  `json:"transaction"`
+    PushURL     string       `json:"push_url"`
+    RedirectURL string       `json:"redirect_url"`
+    Buyer       Buyer        `json:"buyer"`
+    Shipment    Shipment     `json:"shipment"`
+    SellerList  []SellerList `json:"seller_list,omitempty"`
+    BackURL     string       `json:"back_url"`
 }
 
 type Transaction struct {
-	TotalAmount   int        `json:"total_amount"`
-	TransactionID string     `json:"transaction_id"`
-	ItemLists     []ItemList `json:"item_list"`
+    TotalAmount   int        `json:"total_amount"`
+    TransactionID string     `json:"transaction_id"`
+    ItemLists     []ItemList `json:"item_list"`
 }
 
 type ItemList struct {
-	ItemID   string `json:"item_id"`
-	Type     string `json:"type"`
-	Name     string `json:"name"`
-	Price    int    `json:"price"`
-	Quantity int    `json:"quantity"`
-	Category string `json:"category,omitempty"`
-	Url      string `json:"url,omitempty"`
-	SellerID string `json:"seller_id,omitempty"`
+    ItemID   string `json:"item_id"`
+    Type     string `json:"type"`
+    Name     string `json:"name"`
+    Price    int    `json:"price"`
+    Quantity int    `json:"quantity"`
+    Category string `json:"category,omitempty"`
+    Url      string `json:"url,omitempty"`
+    SellerID string `json:"seller_id,omitempty"`
 }
 
 type Buyer struct {
-	Fullname   string `json:"fullname"`
-	Email      string `json:"email"`
-	Phone      string `json:"phone"`
-	Address    string `json:"address"`
-	City       string `json:"city"`
-	District   string `json:"district"`
-	PostalCode string `json:"postal_code"`
-	Company    string `json:"company"`
-	Country    string `json:"country"`
+    Fullname   string `json:"fullname"`
+    Email      string `json:"email"`
+    Phone      string `json:"phone"`
+    Address    string `json:"address"`
+    City       string `json:"city,omitempty"`
+    District   string `json:"district,omitempty"`
+    PostalCode string `json:"postal_code"`
+    Company    string `json:"company,omitempty"`
+    Country    string `json:"country,omitempty"`
 }
 
 type Shipment struct {
-	ShipmentProvider string `json:"shipment_provider"`
-	ShipmentPrice    int    `json:"shipment_price"`
-	ShipmentTax      int    `json:"shipment_tax"`
-	Address          string `json:"address"`
-	City             string `json:"city"`
-	District         string `json:"district"`
-	PostalCode       string `json:"postal_code"`
-	Phone            string `json:"phone"`
-	Company          string `json:"company"`
-	Name             string `json:"name"`
-	Country          string `json:"country"`
+    ShipmentProvider string `json:"shipment_provider"`
+    ShipmentPrice    int    `json:"shipment_price"`
+    ShipmentTax      int    `json:"shipment_tax"`
+    Address          string `json:"address"`
+    City             string `json:"city,omitempty"`
+    District         string `json:"district,omitempty"`
+    PostalCode       string `json:"postal_code"`
+    Phone            string `json:"phone"`
+    Company          string `json:"company,omitempty"`
+    Name             string `json:"name"`
+    Country          string `json:"country,omitempty"`
 }
 
 type SellerList struct {
-	Name  string `json:"name"`
-	Email string `json:"email"`
-	Phone string `json:"phone"`
-	Url   string `json:"url"`
+    Name  string `json:"name"`
+    Email string `json:"email"`
+    Phone string `json:"phone"`
+    Url   string `json:"url"`
 }
 
 type CancelOrderRequest struct {
-	PONumber      string `json:"po_number"`
-	Reason        string `json:"reason"`
-	CancelledBy   string `json:"cancelled_by"`
-	TotalAmount   int    `json:"total_amount"`
-	TransactionID string `json:"transaction_id"`
+    PONumber      string `json:"po_number"`
+    Reason        string `json:"reason"`
+    CancelledBy   string `json:"cancelled_by"`
+    TotalAmount   int    `json:"total_amount"`
+    TransactionID string `json:"transaction_id"`
 }
 
 type UpdateStatusRequest struct {
-	PONumber      string   `json:"po_number"`
-	POStatus      string   `json:"po_status"`
-	TransactionID string   `json:"transaction_id"`
-	Shipment      Shipment `json:"shipment,omitempty"`
-	Reason        string   `json:"reason,omitempty"`
+    PONumber         string `json:"po_number"`
+    POStatus         string `json:"po_status"`
+    TransactionID    string `json:"transaction_id"`
+    Reason           string `json:"reason,omitempty"`
+    ShipmentNo       string `json:"shipment_no"`
+    ShipmentProvider string `json:"shipment_provider"`
 }
